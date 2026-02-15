f5340866df8e9c37d27223fac6c446ed0861026b
Deaw23456 <somjaiyonlawat@gmail.com>
2026-02-15 11:56:33 +0700

    new update

--- pages/user.vue
+++ pages/user.vue
@@ -7,12 +7,7 @@
     </div>
     <common_btn text="Get User List" @click="getUserList" />
     <div class=" tw-grid tw-grid-cols-3 tw-gap-4 tw-px-20 tw-pt-10">
-        <div class=" tw-bg-[#E2E8CE] tw-h-60 tw-flex tw-items-center tw-justify-center tw-rounded-xl tw-shadow-[0_4px_6px_-1px_rgba(0,0,0,0.1),0_2px_4px_-1px_rgba(0,0,0,0.06)] tw-font-bold tw-text-3xl">Test 1</div>
-        <div class=" tw-bg-[#E2E8CE] tw-h-60 tw-flex tw-items-center tw-justify-center tw-rounded-xl tw-shadow-[0_4px_6px_-1px_rgba(0,0,0,0.1),0_2px_4px_-1px_rgba(0,0,0,0.06)] tw-font-bold tw-text-3xl">Test 2</div>
-        <div class=" tw-bg-[#E2E8CE] tw-h-60 tw-flex tw-items-center tw-justify-center tw-rounded-xl tw-shadow-[0_4px_6px_-1px_rgba(0,0,0,0.1),0_2px_4px_-1px_rgba(0,0,0,0.06)] tw-font-bold tw-text-3xl">Test 3</div>
-        <div class=" tw-bg-[#E2E8CE] tw-h-60 tw-flex tw-items-center tw-justify-center tw-rounded-xl tw-shadow-[0_4px_6px_-1px_rgba(0,0,0,0.1),0_2px_4px_-1px_rgba(0,0,0,0.06)] tw-font-bold tw-text-3xl">Test 4</div>
-        <div class=" tw-bg-[#E2E8CE] tw-h-60 tw-flex tw-items-center tw-justify-center tw-rounded-xl tw-shadow-[0_4px_6px_-1px_rgba(0,0,0,0.1),0_2px_4px_-1px_rgba(0,0,0,0.06)] tw-font-bold tw-text-3xl">Test 5</div>
-        <div class=" tw-bg-[#E2E8CE] tw-h-60 tw-flex tw-items-center tw-justify-center tw-rounded-xl tw-shadow-[0_4px_6px_-1px_rgba(0,0,0,0.1),0_2px_4px_-1px_rgba(0,0,0,0.06)] tw-font-bold tw-text-3xl">Test 6</div>
+        <div class=" tw-bg-[#E2E8CE] tw-h-60 tw-flex tw-items-center tw-justify-center tw-rounded-xl tw-shadow-[0_4px_6px_-1px_rgba(0,0,0,0.1),0_2px_4px_-1px_rgba(0,0,0,0.06)] tw-font-bold tw-text-3xl">{{ users }}</div>
     </div>
 </template>
 
@@ -35,7 +30,7 @@
     const getUserList = async () => {
        try{
         const response = await axios.get('https://jsonplaceholder.typicode.com/users',);
-        console.log(response);
+        // console.log(response.data);
         users.value = response.data;
        }catch(error){
         console.error('Error fetching user data:', error);
